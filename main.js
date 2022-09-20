9e2f0278a3e6b9a973aaa322f931955e5b017f66
AtnibamAitay <78244740+AtnibamAitay@users.noreply.github.com>
2022-09-20 09:08:47 +0800

    Add files via upload
    2022.09.20 上传了模式识别与人工智能实验室小程序初版

---
+++ main.js
@@ -0,0 +1,33 @@
+import App from './App'
+import VueDirectiveImagePreviewer from 'vue-directive-image-previewer'
+
+Vue.use(VueDirectiveImagePreviewer)  
+
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+uni.loadFontFace({
+  family: 'ysbth',
+  source: 'url("https://tuanzi-images.oss-cn-guangzhou.aliyuncs.com/106/ysbth.ttf")',
+  global: true,
+  success() {
+	  // console.log('success')
+  }
+})
+
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
